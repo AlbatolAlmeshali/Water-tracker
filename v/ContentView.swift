4725b447ba62c9ef87e953697689cd77be4222ba
Albatol Almeshali <AlbatolAlmeshai@gmail.com>
2023-10-05 12:13:38 +0300

    Initial Commit

---
+++ v/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  v
+//
+//  Created by Albatol Almeshali on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ v/vApp.swift
@@ -0,0 +1,17 @@
+//
+//  vApp.swift
+//  v
+//
+//  Created by Albatol Almeshali on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct vApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
